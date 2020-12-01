63b0524329e63593f55e60beac15af8aa7846a2a
alberto <albertohf91@gmail.com>
2020-12-01 16:54:17 -0300

    First Commit

---
+++ pages/sobre.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+function Sobre() {
+    return (
+        <div>
+            <h1>
+                Sobre Beliscos & Petiscos
+            </h1>
+            <Link href="/">
+                <a>acessar pagina Home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Sobre
